399fa9b070fe7eb8e83f3e9cf688af36c4099c3a
Maxim Andreev <andreevmaxim@gmail.com>
2020-04-23 20:55:23 +0300

    add DataFrameCache class

---
+++ investments/data_providers/cache.py
@@ -0,0 +1,36 @@
+import datetime
+import os
+from typing import Optional
+
+import pandas
+
+
+class DataFrameCache(object):
+    def __init__(self, cache_dir: Optional[str], cache_file: str, ttl: datetime.timedelta):
+        if cache_dir is None:
+            self._cache_file = None
+            return
+
+        assert cache_file is not None
+        assert ttl is not None
+        os.makedirs(cache_dir, exist_ok=True)
+
+        self._cache_file = os.path.join(cache_dir, cache_file)
+        self._ttl = ttl
+
+    def get(self) -> Optional[pandas.DataFrame]:
+        if self._cache_file is None:
+            return None
+        try:
+            mtime = os.path.getmtime(self._cache_file)
+        except FileNotFoundError:
+            return None
+
+        if (datetime.datetime.utcnow() - datetime.datetime.utcfromtimestamp(mtime)) > self._ttl:
+            return None
+        return pandas.read_pickle(self._cache_file)
+
+    def put(self, df: pandas.DataFrame):
+        if self._cache_file is None:
+            return None
+        df.to_pickle(self._cache_file)
--- investments/data_providers/cbr.py
+++ investments/data_providers/cbr.py
@@ -1,49 +1,42 @@
 import datetime
-import os
 import xml.etree.ElementTree as ET
+from typing import List, Tuple
 
 import pandas
 import requests
 
 from investments.currency import Currency
+from investments.data_providers.cache import DataFrameCache
 from investments.money import Money
 
 
 class ExchangeRatesRUB(object):
     def __init__(self, year_from: int = 2000, cache_dir: str = None):
-        today = datetime.datetime.utcnow().date()
-        cache_file = None
-        if cache_dir is not None:
-            os.makedirs(cache_dir, exist_ok=True)
-            cache_file = os.path.join(cache_dir, f'cbrates_since{year_from}.cache')
-            try:
-                mdate = datetime.datetime.utcfromtimestamp(os.path.getmtime(cache_file)).date()
-            except FileNotFoundError:
-                pass
-            else:
-                if today == mdate:
-                    self._df = pandas.read_pickle(cache_file)
-                    return
+        cache = DataFrameCache(cache_dir, f'cbrates_since{year_from}.cache', datetime.timedelta(days=1))
+        df = cache.get()
+        if df is not None:
+            self._df = df
+            return
 
         r = requests.get(f'http://www.cbr.ru/scripts/XML_dynamic.asp?date_req1=01/01/{year_from}&date_req2=01/01/2030&VAL_NM_RQ=R01235')
         tree = ET.fromstring(r.text)
 
-        rates_data = []
+        rates_data: List[Tuple[datetime.date, Money]] = []
         for rec in tree.findall('Record'):
             assert rec.get('Id') == 'R01235', 'only USD(R01235) supported'
-            d = datetime.datetime.strptime(rec.get('Date'), '%d.%m.%Y').date()
-            v = rec.find('Value').text.replace(',', '.')
-            rates_data.append((d, Money(v, Currency.RUB)))
+            d = datetime.datetime.strptime(rec.attrib['Date'], '%d.%m.%Y').date()
+            v = rec.findtext('Value')
+            assert isinstance(v, str)
+            rates_data.append((d, Money(v.replace(',', '.'), Currency.RUB)))
 
         df = pandas.DataFrame(rates_data, columns=['date', 'rate'])
         df.set_index(['date'], inplace=True)
         # df = df.reindex(pandas.date_range(df.index.min(), df.index.max()))
+        today = datetime.datetime.utcnow().date()
         df = df.reindex(pandas.date_range(df.index.min(), today))
         df['rate'].fillna(method='pad', inplace=True)
 
-        if cache_file is not None:
-            df.to_pickle(cache_file)
-
+        cache.put(df)
         self._df = df
 
     def dataframe(self) -> pandas.DataFrame:
